7acc5479bc2ecb42e60aee8d7e64ec083e7c2b2b
rentan-mbp <tan.re60@gmail.com>
2024-07-25 19:13:10 +0800

    user in nav

--- src/components/DropDownBurger.tsx
+++ src/components/DropDownBurger.tsx
@@ -9,8 +9,11 @@ import { Button, buttonVariants } from "./ui/button";
 import { DropdownMenuSubTrigger } from "@radix-ui/react-dropdown-menu";
 import { Grip } from "lucide-react";
 import Link from "next/link";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function DropDownBurger() {
+export default async function DropDownBurger() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -33,7 +36,9 @@ export default function DropDownBurger() {
         <DropdownMenuItem>
           {" "}
           <Link
-            href="/compound_calculator"
+            href={
+              user ? `/compound_calculator/${user?.id}` : `/compound_calculator`
+            }
             className={buttonVariants({
               variant: "ghost",
               size: "sm",
