db3ebfdefd9def6977e77ae1dd4d7225e54831ab
rentan-mbp <tan.re60@gmail.com>
2025-03-19 19:28:54 +0800

    login cookies

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,2 +1,11 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+
+export const GET = handleAuth({
+  session: {
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60, // 30 days
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+});
